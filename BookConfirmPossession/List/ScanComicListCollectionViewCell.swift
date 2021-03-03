9c8905f5a79ba264ecc994ab8bea1cc735c52b10
Astoremica <misawashi4zy@gmail.com>
2021-03-04 02:19:33 +0900

    add editMode scanComicList

--- BookConfirmPossession/List/ScanComicListCollectionViewCell.swift
+++ BookConfirmPossession/List/ScanComicListCollectionViewCell.swift
@@ -11,12 +11,23 @@ class ScanComicListCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var scanComicListCellImageView: UIImageView!
     
-
+    @IBOutlet weak var selectLabel: UILabel!
+    
+    var isEditing:Bool = false{
+        didSet{
+            selectLabel.isHidden = !isEditing
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         self.layer.cornerRadius = 5
+        selectLabel.layer.cornerRadius = 15
+        selectLabel.layer.masksToBounds = true
+        selectLabel.layer.borderColor = UIColor.white.cgColor
+        selectLabel.layer.borderWidth = 1.0
+        selectLabel.layer.backgroundColor = UIColor.black.withAlphaComponent(0.5).cgColor
     }
 
 }
--- BookConfirmPossession/List/ScanComicListViewController.swift
+++ BookConfirmPossession/List/ScanComicListViewController.swift
@@ -16,12 +16,16 @@ class ScanComicListViewController: UIViewController,UICollectionViewDataSource, 
     // スキャンリストUICollectionView
     @IBOutlet weak var scanComicListCollectionView: UICollectionView!
     
-    
-    
-    
-    
+    @IBOutlet weak var scanComicListButton: NeumorphismButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 選択ボタン
+        navigationItem.rightBarButtonItem = editButtonItem
+        navigationItem.rightBarButtonItem?.title = "選択"
+        // ボタン
+        scanComicListButton.setTitle("買う", for: .normal)
+        scanComicListButton.setTitleColor(UIColor(hex: "4966FF"), for: .normal)
+        
         if traitCollection.userInterfaceStyle == .dark {
             view.backgroundColor = UIColor(displayP3Red: 85/255, green: 85/255, blue: 85/255,alpha: 1.0)
             scanComicListCollectionView.backgroundColor = UIColor(displayP3Red: 85/255, green: 85/255, blue: 85/255,alpha: 1.0)
@@ -29,7 +33,6 @@ class ScanComicListViewController: UIViewController,UICollectionViewDataSource, 
             view.backgroundColor = UIColor(displayP3Red: 241/255, green: 241/255, blue: 241/255,alpha: 1.0)
             scanComicListCollectionView.backgroundColor = UIColor(displayP3Red: 241/255, green: 241/255, blue: 241/255,alpha: 1.0)
         }
-//        let getComicList = userDefaults.array(forKey: "comics") as? [[String:String]]
         
         collectionLayout.minimumLineSpacing = 20
         collectionLayout.minimumInteritemSpacing = 20
@@ -52,22 +55,53 @@ class ScanComicListViewController: UIViewController,UICollectionViewDataSource, 
         if let getComicList = userDefaults.array(forKey: "comics") as? [[String:String]]{
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ScanComicListCollectionViewCell", for: indexPath) as! ScanComicListCollectionViewCell
             cell.scanComicListCellImageView.downloaded(from: getComicList[indexPath.row]["cover"]!)
+            cell.isEditing = isEditing
             return cell
         }else{
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ScanComicListCollectionViewCell", for: indexPath) as! ScanComicListCollectionViewCell
-//            cell.scanComicListCellImageView.image = UIImage(named: "photo2")
             cell.backgroundColor = UIColor.red
             return cell
         }
         
-        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,didSelectItemAt indexPath: IndexPath) {
+        if !isEditing{
+            print(indexPath.row)
+        }
         
     }
     
-    func collectionView(_ collectionView: UICollectionView,
-                        didSelectItemAt indexPath: IndexPath) {
-        print(indexPath.row)
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        scanComicListCollectionView.allowsMultipleSelection = editing
+        scanComicListCollectionView.indexPathsForVisibleItems.forEach{(indexPath) in
+            let cell = scanComicListCollectionView.cellForItem(at: indexPath) as! ScanComicListCollectionViewCell
+            cell.isEditing = editing
+             
+        }
+        if editing {
+            scanComicListButton.setTitle("削除", for: .normal)
+            scanComicListButton.setTitleColor(UIColor(hex: "FF6363"), for: .normal)
+        }else{
+            scanComicListButton.setTitle("買う", for: .normal)
+            scanComicListButton.setTitleColor(UIColor(hex: "4966FF"), for: .normal)
+        }
     }
+    
+
+    
+//    @IBAction func listChangeMode(_ sender: Any) {
+//        if scanComicListCollectionView.isEditing == true{
+//            scanComicListCollectionView.isEditing = false
+//            print("通常")
+//        }else{
+//            scanComicListCollectionView.isEditing = true
+//            print("編集")
+//        }
+//    }
+    
+    
     /*
      // MARK: - Navigation
      
