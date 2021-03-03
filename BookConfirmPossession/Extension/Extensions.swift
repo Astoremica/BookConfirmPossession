ac68e867e43b4863681ed617ca176a44ad947554
Astoremica <misawashi4zy@gmail.com>
2021-03-03 19:50:39 +0900

    Extensionを1ファイルに

--- BookConfirmPossession/Extension/Extensions.swift
+++ BookConfirmPossession/Extension/Extensions.swift
@@ -1,11 +1,12 @@
 //
-//  GetImageByURL.swift
+//  Extensions.swift
 //  BookConfirmPossession
 //
 //  Created by YoNa on 2021/03/01.
 //
 import  UIKit
 
+/// URLからImageViewに反映
 extension UIImageView {
     func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
         contentMode = mode
@@ -26,12 +27,33 @@ extension UIImageView {
         downloaded(from: url, contentMode: mode)
     }
 }
-
+/// 指定文字置き換え
 extension String {
     func replace(_ from: String,_ to: String) -> String {
         var replacedString = self
         replacedString = replacedString.replacingOccurrences(of: from, with: to)
 
         return replacedString
+    }
+}
+
+/// UIViewController拡張(アラート)
+public extension UIViewController {
+
+    func showAlert(title: String, message: String, actions: [UIAlertAction]) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        actions.forEach { alert.addAction($0) }
+        present(alert, animated: true)
+    }
+}
+
+/// Hex to RGB
+extension UIColor {
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        let v = Int("000000" + hex, radix: 16) ?? 0
+        let r = CGFloat(v / Int(powf(256, 2)) % 256) / 255
+        let g = CGFloat(v / Int(powf(256, 1)) % 256) / 255
+        let b = CGFloat(v / Int(powf(256, 0)) % 256) / 255
+        self.init(red: r, green: g, blue: b, alpha: min(max(alpha, 0), 1))
     }
 }
